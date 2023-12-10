814cae9eee5ef612ef8a9dade1fb226ccec34e76
Arpan Das <arpan.das@klythe.com>
2023-12-10 22:39:58 +0530

    app logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
+  const data = { temperature: 25, conditions: "Sunny" };
+  let Threshold = 20;
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      {/* Do not remove the main div */}
+      <h1>
+        Temperature:
+        <b style={{ color: data.temperature > Threshold ? "red" : "blue" }}>
+          {data.temperature}
+        </b>
+      </h1>
+      <h1>conditions:{data.conditions}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
